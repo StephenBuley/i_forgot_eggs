5b76643ceed940048b966a200f2ff4f6057c50f5
Stephen Buley <stephen.a.buley@gmail.com>
2024-08-15 15:58:33 +0200

    feat: add swipe for new list functionality with PageView

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<MyHomePage> {
-  AppList list = AppList(id: 1, title: 'Grocery List');
+  @override
+  Widget build(BuildContext context) {
+    return const PagesOfAppLists();
+  }
+}
+
+class PagesOfAppLists extends StatefulWidget {
+  const PagesOfAppLists({super.key});
+
+  @override
+  State<PagesOfAppLists> createState() => _PagesOfAppListsState();
+}
+
+class _PagesOfAppListsState extends State<PagesOfAppLists> {
+  List<AppList> lists = [
+    AppList(id: 1, title: 'Grocery List'),
+  ];
 
   @override
   void initState() {
+    final list = lists[0];
     list.listItems.add(ListItem(id: 1, text: 'Eggs'));
     list.listItems.add(ListItem(id: 2, text: 'Milk'));
     list.listItems.add(ListItem(id: 3, text: 'Salsa'));
@@ -46,29 +63,121 @@ class HomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('I Forgot Eggs'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.ios_share),
+          ),
+        ],
+      ),
+      body: PageView.builder(
+        itemCount: lists.length + 1, // +1 for the "add new list" page
+        itemBuilder: (context, index) {
+          if (index < lists.length) {
+            return AppListPage(
+              list: lists[index],
+              onListUpdated: (updatedlist) {
+                setState(() {
+                  lists[index] = updatedlist;
+                });
+              },
+            );
+          } else {
+            return Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    lists.add(AppList(
+                      id: 1,
+                      title: 'new list',
+                    ));
+                  });
+                },
+                child: const Text('Add New List'),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class AppListPage extends StatefulWidget {
+  final AppList list;
+  final Function(AppList) onListUpdated;
+
+  const AppListPage(
+      {super.key, required this.list, required this.onListUpdated});
+
+  @override
+  State<AppListPage> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<AppListPage> {
+  late AppList list;
+
+  @override
+  void initState() {
+    super.initState();
+    list = widget.list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
         child: ReorderableListView(
-          buildDefaultDragHandles: false,
+          header: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              list.title,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
           prototypeItem: CheckboxListTile(
             value: false,
             onChanged: (value) {},
+            title: const Text("Sample Item"),
           ),
-          children: list.listItems.map(
-            (item) {
-              return CheckboxListTile(
-                key: ValueKey(item.id),
-                value: item.completed,
-                controlAffinity: ListTileControlAffinity.leading,
+          children: list.listItems.map((item) {
+            return CheckboxListTile(
+              key: ValueKey(item.id),
+              value: item.completed,
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (value) {
+                setState(() {
+                  item.completed = value ?? false;
+                  widget.onListUpdated(list);
+                });
+              },
+              title: TextFormField(
+                enableSuggestions: false,
+                initialValue: item.text,
                 onChanged: (value) {
                   setState(() {
-                    item.completed = value!;
+                    item.text = value;
+                    widget.onListUpdated(list);
                   });
                 },
-                title: Text(item.text),
-              );
-            },
-          ).toList(),
-          onReorder: (oldIndex, newIndex) {},
+              ),
+            );
+          }).toList(),
+          onReorder: (oldIndex, newIndex) {
+            setState(() {
+              if (oldIndex < newIndex) {
+                newIndex -= 1;
+              }
+              final movedItem = list.listItems.removeAt(oldIndex);
+              list.listItems.insert(newIndex, movedItem);
+              widget.onListUpdated(list);
+            });
+          },
         ),
       ),
     );
